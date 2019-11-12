d95202d1ae0521d4d8d359a9d2c484b2aaddcd57
Andre Patacas <andrepatacas@ua.pt>
2019-11-12 23:42:37 +0000

    Finalização da implementação da classe de teste do BloomFilter e melhorias na classe BloomFilter

--- Projeto-MPEI/src/BloomFilter.java
+++ Projeto-MPEI/src/BloomFilter.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class BloomFilter {
     private int n, k;
     private boolean[] filter;
@@ -7,18 +12,68 @@ public class BloomFilter {
         this.k = k;
         this.filter = new boolean[n];
     }
-    void addElement(String elem){
-        for(var i = 0; i < k; i++){
-            filter[Math.abs((elem + i).hashCode() % n)] = true;
+
+
+    public static int optimalK(int n, int m) {
+        return optimalK((long) n, (long) m);
+    }
+
+    public static int optimalK(long n, long m) {
+        int optK = (int) Math.round(n * 0.693 / m);
+        return optK == 0 ? 1 : optK;
+    }
+
+    public static BloomFilter fromFile(String fName) {
+        File book = new File(fName);
+        Set<String> wordSet = new HashSet<String>();
+        try (Scanner k = new Scanner(book)) {
+            while (k.hasNext()) {
+                wordSet.add(k.next());
+            }
+        } catch (Exception e) {
+            System.out.println("Runtime Error: " + e.toString());
+        }
+        var b = new BloomFilter(wordSet.size() * 4, BloomFilter.optimalK(wordSet.size() * 8, wordSet.size()));
+        for (var elem : wordSet) {
+            b.addElement(elem);
+        }
+        return b;
+    }
+
+    public double probErr(int numElem) {
+        return Math.pow(1 - Math.pow(1 - (double) 1 / n, k * numElem), k);
+    }
+
+    void addElement(String elem) {
+        var cond = true;
+        for (var i = 0; i < k; i++) {
+            if (cond) filter[Math.abs((elem + i).hashCode() % n)] = true;
+            else filter[string2hash(elem, n, i)] = true;
+            cond = !cond;
         }
     }
-    boolean isElement(String elem){
-        for(var i = 0; i < k; i++){
-            if(!filter[Math.abs((elem + i).hashCode() % n)]){
+
+    boolean isElement(String elem) {
+        var cond = true;
+        for (var i = 0; i < k; i++) {
+            if (cond && !filter[Math.abs((elem + i).hashCode() % n)]) {
+                return false;
+            } else if (!cond && !filter[string2hash(elem, n, i)]) {
                 return false;
             }
+            cond = !cond;
         }
         return true;
+    }
+
+    private int string2hash(String s, int mod, int seed) {
+        long result = (long) Math.pow(seed, seed * 3);
+        char[] ca = s.toCharArray();
+        for (var c : ca) {
+            result = result * 31 + c;
+        }
+        result = Math.abs(result);
+        return (int) (result % mod);
     }
 
     public int getN() {
--- Projeto-MPEI/src/BloomFilterTest.java
+++ Projeto-MPEI/src/BloomFilterTest.java
@@ -1,19 +1,83 @@
-import java.nio.charset.Charset;
-import java.util.Arrays;
-import java.util.Random;
+import java.io.File;
+import java.util.*;
 
 public class BloomFilterTest {
+    static int testWeight = 100;
+    static double accuracy = .1;
+
     public static void main(String[] args) {
-        for (var j = 0; j < 20; j++) {
-            var b = new BloomFilter(100000 * 8, j);
-            for (var i = 0; i < 100000; i++)
-                b.addElement(randomString());
-            var count = 0;
-            for (var i = 0; i < 100000; i++) {
-                if (b.isElement(randomString())) count++;
+        optimalKFullTest();
+        BloomFilter b = BloomFilter.fromFile("mises.txt");
+        falseNegativeTest(b);
+        falsePositiveFullTest(b);
+    }
+
+    private static void falsePositiveFullTest(BloomFilter b) {
+        System.out.print("Teste de falsos positivos em execução: ");
+        File book = new File("mises.txt");
+        Set<String> wordSet = new HashSet<String>();
+        try (Scanner k = new Scanner(book)) {
+            while (k.hasNext()) {
+                wordSet.add(k.next());
             }
-            System.out.println((double) count / 100000);
+        } catch (Exception e) {
+            System.out.println("Runtime Error: " + e.toString());
         }
+        var numErros = 0;
+        for (var i = 0; i < testWeight; i++) {
+            var result = falsePositiveTest(b);
+            if (Math.abs(result - b.probErr(wordSet.size())) > .1) numErros++;
+            if (i % (testWeight / 10) == 0) {
+                System.out.printf("%3.3s, ", ((double) i / testWeight));
+            }
+        }
+        System.out.println();
+        if (numErros != 0 && ((double) numErros / testWeight) > accuracy) {
+            System.out.println("Diferença entre o valor teorico e pratico de falsos positivos consistentemente alta");
+            System.out.printf("%s erros em %s testes", numErros, testWeight);
+        }else{
+            System.out.println("Passou !");
+        }
+    }
+
+    private static double falsePositiveTest(BloomFilter b) {
+        int count = 0;
+        for (var i = 0; i < 10000; i++) {
+            if (b.isElement(randomString())) count++;
+        }
+        var err = (double) count / 10000;
+        return err;
+    }
+
+    private static void falseNegativeTest(BloomFilter b) {
+        System.out.print("Teste de falsos negativos em execução ");
+        File book = new File("mises.txt");
+        Set<String> wordSet = new HashSet<String>();
+        try (Scanner k = new Scanner(book)) {
+            while (k.hasNext()) {
+                wordSet.add(k.next());
+            }
+        } catch (Exception e) {
+            System.out.println("Runtime Error: " + e.toString());
+        }
+        var erro = false;
+        for (var elem : wordSet) {
+            if (!b.isElement(elem)) erro = true;
+        }
+        System.out.println();
+        if(erro){
+            System.out.println("Falso Negativo Detetado");
+        }else{
+            System.out.println("Passou !");
+        }
+    }
+
+
+    private static BloomFilter createRandomFilter(int n, int k, int numElem) {
+        var b = new BloomFilter(n, k);
+        for (var i = 0; i < numElem; i++)
+            b.addElement(randomString());
+        return b;
     }
 
     public static String randomString() {
@@ -29,4 +93,55 @@ public class BloomFilterTest {
         }
         return buffer.toString();
     }
+
+
+    private static void optimalKFullTest() {
+        System.out.print("Teste de falsos positivos em execução: ");
+        var numErros = 0;
+        for (var i = 0; i < testWeight; i++) {
+            int r1 = (int) Math.round(Math.random() * 10000);
+            int r2 = (int) Math.round(Math.random() * r1);
+            var result = optimalKTest(r1, r2);
+            if (Math.abs(result.experimental - result.teorico) > 2) numErros++;
+            if (i % (testWeight / 10) == 0) {
+                System.out.printf("%3.3s, ", ((double) i / testWeight));
+            }
+        }
+        System.out.println();
+        if (numErros != 0 && ((double) numErros / testWeight) > accuracy) {
+            System.out.println("Erro na função de K otimo");
+            System.out.printf("%s erros em %s testes", numErros, testWeight);
+        } else {
+            System.out.println("Passou !");
+        }
+    }
+
+    private static TestResult optimalKTest(int n, int numElem) {
+        double c_best_err = 1;
+        var c_best_index = -1;
+        for (var j = 1; j < 10; j++) {
+            var b = createRandomFilter(n, j, numElem);
+            var count = 0;
+            for (var i = 0; i < 10000; i++) {
+                if (b.isElement(randomString())) count++;
+            }
+            var err = (double) count / 10000;
+            if (c_best_err > err) {
+                c_best_err = err;
+                c_best_index = j;
+            }
+        }
+        if (c_best_err == 0 || BloomFilter.optimalK(n, numElem) > 10) return new TestResult(0, 0);
+        return new TestResult(c_best_index, BloomFilter.optimalK(n, numElem));
+    }
+
+}
+
+class TestResult {
+    public TestResult(double teorico, double experimental) {
+        this.teorico = teorico;
+        this.experimental = experimental;
+    }
+
+    double teorico, experimental;
 }
