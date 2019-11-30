edf05dee079d5ccc8d0c505daf9320226d13e0ec
Sofia Vaz <sofiateixeiravaz@ua.pt>
2019-11-30 16:59:59 +0000

    testing MinHash (final?)

--- Projeto-MPEI/src/Tests/MinHashTest.java
+++ Projeto-MPEI/src/Tests/MinHashTest.java
@@ -2,35 +2,33 @@ package Tests;
 
 import modules.MinHash;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 
 class MinHashTest {
     public static void main(String[] args) {
         int values = Integer.MAX_VALUE / 100;
-        int leftBound = Integer.MIN_VALUE/100;
-        int rightBound = Integer.MAX_VALUE/100;
+        int leftBound = Integer.MIN_VALUE / 100;
+        int rightBound = Integer.MAX_VALUE / 100;
         long totalSize = rightBound + Math.abs(leftBound);
         int numberOfDivisions = 1000;
-        long sizeOfDivision = (int) (totalSize/numberOfDivisions);
+        long sizeOfDivision = (int) (totalSize / numberOfDivisions);
         var divisions = new int[numberOfDivisions];
-        for (int i = 0; i < numberOfDivisions; i++) {
-            divisions[i] = 0;
-        }
+        Arrays.fill(divisions, 0);
         for (int i = 0; i < values; i++) {
             var valueList = new LinkedList<Integer>();
-            valueList.add((int) Math.random() * Integer.MAX_VALUE);
-            int hash =new MinHash(valueList).getSignature()[0];
+            valueList.add((int) ( Math.random() * Integer.MAX_VALUE));
+            var hashSignature = new MinHash(valueList).getSignature();
+            Arrays.sort(hashSignature);
+            var hash = hashSignature[0];
             int div = 0; //div being the index of the division that this element is a part of
-            while((leftBound + div*sizeOfDivision) < hash) {
+            while ((leftBound + (div * sizeOfDivision)) < hash) {
                 div++;
             }
             divisions[div--]++;
-            System.out.println(i+"th element added to "+div+"division");
         }
 
-        for (int i = 0; i < numberOfDivisions; i++) {
-            System.out.println(divisions[i] * 100 / totalSize + "%");
-        }
+        System.out.println(Arrays.toString(divisions));
     }
 }
 
